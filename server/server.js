26000e2e121fb321ecb03a7e242697ead7adf463
Yuki Haita <hatsuisa0921@gmail.com>
2018-08-07 12:47:04 +0900

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+const {ObjectID} = require('mongodb')
+
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
 
 var app = express();
 
@@ -28,6 +30,21 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   })
 });
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send('Id not valid');
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(400).send('Todo not found');
+    }
+    res.send({todo});
+  })
+})
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
