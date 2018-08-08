6d0a09cb988c69f0ed245f89db332ce9547511ff
Yuki Haita <hatsuisa0921@gmail.com>
2018-08-08 19:44:08 +0900

    Associate users with Todo

--- playground/hashing.js
+++ playground/hashing.js
@@ -4,15 +4,15 @@ const bcrypt = require('bcryptjs');
 
 var password = 'abc123';
 
-bcrypt.genSalt(10, (err, salt) => {
-  bcrypt.hash(password, salt, (err, hash) => {
-    console.log(hash);
-  })
-});
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   })
+// });
 
 var hashedPassword = '$2a$10$b4OJ3n75R2YVhEYpS.Axnu.U6MCLHsCzfWHn50UBtK.680RMIpyDm';
 
-bcrypt.compare('abc1234', hashedPassword, (err, result) => {
+bcrypt.compare('abc123', hashedPassword, (err, result) => {
   console.log(result);
 });
 
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
 const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
@@ -17,9 +18,10 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
   var todo = new Todo({
-    text: req.body.text
+    text: req.body.text,
+    _creator: req.user._id
   });
 
   todo.save().then((doc) => {
@@ -29,22 +31,27 @@ app.post('/todos', (req, res) => {
   })
 });
 
-app.get('/todos', (req, res) => {
-  Todo.find().then((todos) => {
+app.get('/todos', authenticate, (req, res) => {
+  Todo.find({
+    _creator: req.user._id
+  }).then((todos) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
   })
 });
 
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
     return res.status(404).send('Id not valid');
   }
 
-  Todo.findById(id).then((todo) => {
+  Todo.findOne({
+    _id: id,
+    _creator: req.user._id
+  }).then((todo) => {
     if (!todo) {
       return res.status(400).send('Todo not found');
     }
@@ -52,14 +59,17 @@ app.get('/todos/:id', (req, res) => {
   })
 })
 
-app.delete('/todos/:id', (req, res) => {
+app.delete('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
 
-  Todo.findByIdAndRemove(id).then((todo) => {
+  Todo.findOneAndRemove({
+    _id: id,
+    _creator: req.user._id
+  }).then((todo) => {
     if (!todo) {
       return res.status(404).send();
     }
@@ -70,7 +80,7 @@ app.delete('/todos/:id', (req, res) => {
   })
 })
 
-app.patch('/todos/:id', (req, res) => {
+app.patch('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
   var body = _.pick(req.body, ['text', 'completed']);
 
@@ -85,7 +95,7 @@ app.patch('/todos/:id', (req, res) => {
     body.completedAt = null;
   }
 
-  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+  Todo.findOneAndUpdate({_id: id, _creator: req.user._id}, {$set: body}, {new: true}).then((todo) => {
     if (!todo) {
       return res.status(404).send();
     }
@@ -100,6 +110,7 @@ app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
 
+  // save()でvalidationが実行される
   user.save().then(() => {
     return user.generateAuthToken();
     // res.send(user);
@@ -107,11 +118,46 @@ app.post('/users', (req, res) => {
     res.header('x-auth', token).send(user);
   }).catch((e) => {
     res.status(400).send(e);
-  })
+  });
 })
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+})
+
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    })
+  }).catch((e) => {
+    res.send(400).send();
+  });
+
+  // var email = req.body.email;
+  // var password = req.body.password;
+  // User.findOne({email}).then((user) => {
+  //   var hashedPassword = user.password;
+  //   bcrypt.compare(password, hashedPassword, (err, result) => {
+  //     if (result) {
+  //       res.header('x-auth', user.tokens[0].token).send(user);
+  //     } else {
+  //       res.status(401).send('Incorrect password');
+  //     }
+  //   });
+  // }).catch((e) => {
+  //   res.status(400).send('User not found');
+  // })
+})
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  })
 })
 
 // app.post('/todos', (req, res) => {
